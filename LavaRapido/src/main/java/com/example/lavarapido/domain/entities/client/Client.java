3bbb69b989b43f2252e476079539afe833c5aa2d
Luan1611 <luan_1611@hotmail.com>
2024-05-16 14:42:07 -0300

    creation of Client, Telephone and CPF classes

--- LavaRapido/src/main/java/com/example/lavarapido/domain/entities/client/Client.java
+++ LavaRapido/src/main/java/com/example/lavarapido/domain/entities/client/Client.java
@@ -1,4 +1,44 @@
 package com.example.lavarapido.domain.entities.client;
 
 public class Client {
+
+    private long id;
+
+    private String name;
+
+
+    public Client() {};
+
+
+    public void changeClientStatus(Status status) {
+
+    }
+
+    public void addVehicle(VehicleDTO vDTO) {
+
+    }
+
+    public void deleteVehicle(VehicleDTO vDTO) {
+
+    }
+
+    public void changeVehicleStatus(long idVehicle, Status status) {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- LavaRapido/src/main/java/com/example/lavarapido/domain/entities/client/Telephone.java
+++ LavaRapido/src/main/java/com/example/lavarapido/domain/entities/client/Telephone.java
@@ -1,4 +1,11 @@
 package com.example.lavarapido.domain.entities.client;
 
 public class Telephone {
+
+    private String telephone;
+
+    public String getTelephone() {
+        return telephone;
+    }
+
 }
