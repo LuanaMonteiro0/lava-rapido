deb341b05cf56d6a91d34ddb19eebba37edbe916
Luan1611 <luan_1611@hotmail.com>
2024-05-16 14:55:18 -0300

    add atributes on Client Class

--- LavaRapido/src/main/java/com/example/lavarapido/domain/entities/client/Client.java
+++ LavaRapido/src/main/java/com/example/lavarapido/domain/entities/client/Client.java
@@ -1,11 +1,26 @@
 package com.example.lavarapido.domain.entities.client;
 
+import com.example.lavarapido.domain.entities.general.Status;
+import com.example.lavarapido.domain.entities.scheduling.Scheduling;
+import com.example.lavarapido.domain.entities.vehicle.Vehicle;
+
+import java.util.List;
+
 public class Client {
 
     private long id;
 
     private String name;
 
+    private Telephone phone;
+
+    private CPF cpf;
+
+    private List<Vehicle> vehicles;
+
+    private Status status; // Tem valor padrão?
+
+    private List<Scheduling> schedulings;
 
     public Client() {};
 
