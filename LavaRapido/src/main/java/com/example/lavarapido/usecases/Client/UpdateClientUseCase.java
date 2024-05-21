1a462767a74e3d208fb59c5c85aca22321d33827
Isabela de Ponte <isabelapnt01@gmail.com>
2024-05-21 16:53:57 -0300

    feat: implements update client use case

---
+++ LavaRapido/src/main/java/com/example/lavarapido/usecases/Client/UpdateClientUseCase.java
@@ -0,0 +1,35 @@
+package com.example.lavarapido.usecases.Client;
+
+import com.example.lavarapido.domain.entities.client.Client;
+import com.example.lavarapido.usecases.utils.EntityNotFoundException;
+import com.example.lavarapido.usecases.utils.Notification;
+import com.example.lavarapido.usecases.utils.Validator;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UpdateClientUseCase {
+    private final ClientDAO clientDAO;
+
+    public UpdateClientUseCase(ClientDAO clientDAO) {
+        this.clientDAO = clientDAO;
+    }
+
+    public Optional<List<Client>> findClientsByName(String name) {
+        return clientDAO.findByName(name);
+    }
+
+    public boolean update(Client client) {
+        Validator<Client> validator = new ClientInputRequestValidator();
+        Notification notification = validator.validate(client);
+
+        if (notification.hasErrors())
+            throw new IllegalArgumentException(notification.errorMessage());
+
+        Integer id = client.getId();
+        if (clientDAO.findOne(String.valueOf(id)).isEmpty())
+            throw new EntityNotFoundException("Client not found.");
+
+        return clientDAO.update(client);
+    }
+}
