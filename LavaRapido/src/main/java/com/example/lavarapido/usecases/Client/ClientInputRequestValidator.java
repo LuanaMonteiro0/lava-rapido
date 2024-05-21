248c16ab0d9ef3298a027e1197b394dce0929e7e
Isabela de Ponte <isabelapnt01@gmail.com>
2024-05-21 16:25:44 -0300

    feat: implements client subclasses

---
+++ LavaRapido/src/main/java/com/example/lavarapido/usecases/Client/ClientInputRequestValidator.java
@@ -0,0 +1,28 @@
+package com.example.lavarapido.usecases.Client;
+
+import com.example.lavarapido.domain.entities.client.Client;
+import com.example.lavarapido.usecases.utils.Notification;
+import com.example.lavarapido.usecases.utils.Validator;
+
+public class ClientInputRequestValidator extends Validator<Client> {
+    @Override
+    public Notification validate(Client client) {
+
+        Notification notification = new Notification();
+
+        if (client == null) {
+            notification.addError("Client is null");
+            return notification;
+        }
+
+        if (nullOrEmpty(client.getName()))
+            notification.addError("Name is null or empty");
+        if (nullOrEmpty(client.getCpfString()))
+            notification.addError("CPF is null or empty");
+        if (nullOrEmpty(client.getPhone()))
+            notification.addError("Phone is null or empty");
+
+
+        return null;
+    }
+}
